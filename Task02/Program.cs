050e6d3a053555485dcc5b52910e059e25576132
Evgeniy <iva.sakharov.e@gmail.com>
2022-12-18 21:26:39 +0300

    Task2 complete

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,3 +4,15 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше {number2}");
+}
+else
+    Console.WriteLine($"Число {number2} больше {number1}");
