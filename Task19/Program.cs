8c68c863d4d2e6afe3b13a5df3e0f1613692fc55
Evgeniy <iva.sakharov.e@gmail.com>
2022-12-25 19:56:51 +0300

    Task 19 complete

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,14 +8,18 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number>=10000&&number<=99999)
+
+if (number >= 10000 && number <= 99999)
+{
+    Console.Write($"{number} --> ");
+    Console.WriteLine(Palindrome(number) ? "Да" : "Нет");
+}
+else Console.WriteLine("Число не пятизначное");
 
 
 
-
-
-
-
-
-
-
+bool Palindrome(int num)
+{
+    if (num / 10000 == num % 10 & num / 1000 % 10 == num / 10 % 10) return true;
+    else return false;
+}
