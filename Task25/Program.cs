f5fb852f251631b1ccc9b56eb89299d5417f5cb7
Evgeniy <iva.sakharov.e@gmail.com>
2023-01-19 00:08:20 +0300

    Task 25 complete

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,13 +5,27 @@
 // 2, 4 -> 16
 
 
-Console.WriteLine ("Введите число N: ");
-int number = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-void PrintDegree (int num1, int num2)
+void PrintDegree(int num1, int num2) //принимаем на вход числа A и B
 {
-    int num1 = 
+    if (num1 > 0 && num2 > 0)
+    {
+        int count = num1;  //вводим новую переменную
+        for (int i = 2; i <= num2; i++)  //счетчик начинаем с 2х, т.к. это минимальная степень для возведения
+        {
+            count = count * num1;   //возводим число A в степень B        
+        }
+        Console.WriteLine(count);   // выводим результат    
+    }
+    else Console.WriteLine("Введены не натуральные числа");
 }
+
+
+
+
+Console.WriteLine("Введите число A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+PrintDegree(number1, number2);
