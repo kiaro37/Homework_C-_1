58780c68665720cd5f221c092d395df4434f8732
Evgeniy <iva.sakharov.e@gmail.com>
2023-01-20 23:35:39 +0300

    Task 38 complete

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -4,3 +4,55 @@
 
 
 
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(num, 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+double MaxArrayValue(double[] arr) //находим максимальное значение в массиве
+{
+    double maxArr = arr[0];   //присваиваем максимальное значение первому элементу
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > maxArr) maxArr = arr[i];
+    }
+    return maxArr;
+}
+
+double MinArrayValue(double[] arr)   //находим минимальное значение в массиве
+{
+    double minArr = arr[0];   //присваиваем минимальное значение первому элементу
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < minArr) minArr = arr[i];
+    }
+    return minArr;
+}
+
+double[] array = CreateArrayRndDouble(8, 0, 99);
+PrintArray(array);
+Console.WriteLine();
+double maxArray = MaxArrayValue(array);
+double minArray = MinArrayValue(array);
+Console.WriteLine($"Разница между максимальным и минимальным значениями массива --> {maxArray - minArray}");
+
+
+
