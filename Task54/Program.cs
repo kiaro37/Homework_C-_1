2b564d1708f6c5096418edc60971a83b801d1fd4
Evgeniy <iva.sakharov.e@gmail.com>
2023-01-31 19:40:27 +0300

    Task 64 complete

---
+++ Task54/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],7} ");
+        }
+        Console.WriteLine("  |");
+    }
+
+}
+
+int SortMinMaxMatrix(int[,] matrix)
+{
+    int row, col;
+    int[,] newMatrix;
+    for (row = 0; row < matrix.GetLength(0); row++)
+    {
+        Array.Sort(newMatrix[row]);
+    }
+    return newMatrix;
+}
+
+
+int[,] matrix2D = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(matrix2D);
+Console.WriteLine();
+SortMinMaxMatrix(matrix2D);
