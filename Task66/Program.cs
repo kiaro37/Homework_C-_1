396c9233f7c723efe36bedc303ec6c6fb87ff0ab
Evgeniy <iva.sakharov.e@gmail.com>
2023-02-01 11:49:10 +0300

    Task 66 complete

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -4,3 +4,36 @@
 // M = 4; N = 8. -> 30
 
 
+Console.WriteLine("Введите первое число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {numberM}; N = {numberN} --> {SumMinToMax(numberM, numberN)}");
+
+
+
+
+
+int SumMinToMax(int m, int n)
+{
+    if (m < 0) m = 0;
+    {
+        if (n < 0) n = 0;
+        {
+            if (m < 0 && n < 0) return n;
+            {
+                if (m > n)
+                {
+                    if (m == n) return n;
+                    return SumMinToMax(m - 1, n) + m;
+                }
+                if (m < n)
+                {
+                    if (m == n) return n;
+                    return SumMinToMax(m + 1, n) + m;
+                }
+                else return n;
+            }
+        }
+    }
+}
